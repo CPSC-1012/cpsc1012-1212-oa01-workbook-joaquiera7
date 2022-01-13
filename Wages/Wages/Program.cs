8c448f105c073a368f59e2cdfa2fa5ff97d07edb
Jongie Oaquiera <joaquiera1@nait.ca>
2022-01-12 18:36:45 -0700

    Initial commit
    Finished exercise 3

---
+++ Wages/Wages/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Wages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double regularWages;        //The calculated regular wages
+            double basePay = 25;        //The bvase pay rate           
+            double regularHours = 40;   //The hours worked less overtime
+            double overtimeWages;       //Overtime wages
+            double overtimePay = 37.5;  //Overtime pay rate
+            double overtimeHours = 10; //Overtime hours worked
+            double totalWages;          //Total wages
+
+            //calculate regularWages
+            regularWages = basePay * regularHours;
+
+            //calculate overtimeWages
+            overtimeWages = overtimePay * overtimeHours;
+
+            //calculate totalWages
+            totalWages = regularWages + overtimeWages;
+            Console.WriteLine($"Wages for this week is {totalWages:c1}");
+        }
+    }
+}
