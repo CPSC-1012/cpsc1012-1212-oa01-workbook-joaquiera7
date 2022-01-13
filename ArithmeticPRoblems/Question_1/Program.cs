a7068381ba7d8aacabfbc39a4303f16d5343c9f1
Jongie Oaquiera <joaquiera1@nait.ca>
2022-01-13 16:33:28 -0700

    Question 1 done.

---
+++ ArithmeticPRoblems/Question_1/Program.cs
@@ -0,0 +1,35 @@
+﻿//  Purpose:    Write a program that will prompt the user for three real numbers and computes 
+//              their mean average. Display the average to the user.
+//  input = firstNumber, secondNumber, thirdNumber
+//  output = meanAverage
+//Step 1) 
+
+using System;
+
+namespace Question_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mean Average Calculator\n");
+            //get user input
+            Console.Write("Enter the first real number: ");
+            double firstNumber = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter thge second real number: ");
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the third real number: ");
+            double thirdNumber = double.Parse(Console.ReadLine());
+
+            //calculate
+            double meanAverage = (firstNumber + secondNumber + thirdNumber) / 3;
+
+            //display result
+            Console.WriteLine($"\nThe average is {meanAverage}");
+            Console.ReadLine();
+
+        }
+    }
+}
