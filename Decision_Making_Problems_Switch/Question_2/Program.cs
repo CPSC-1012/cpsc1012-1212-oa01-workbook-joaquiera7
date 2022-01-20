a4ada28f911eb4aa6407a854651b2374bbd806d4
Jongie Oaquiera <joaquiera1@nait.ca>
2022-01-19 18:19:50 -0700

    -added converter for pounds <--> kilogram
    -prompt user input

--- Decision_Making_Problems_Switch/Question_2/Program.cs
+++ Decision_Making_Problems_Switch/Question_2/Program.cs
@@ -2,7 +2,7 @@
  *          B)Write a program that can allow the user to perform some unit            conversion.
  *
  *input:    unitConverter (depending on which converter the user want use, 
-            user can input the following; pounds, kilograms, liters, fluidOunces, inches, centimeter, fahrenheit, celsius) 
+            user can input the following; pounds, kilograms, liters, fluidOunces, inches, centimeter, fahrenheit, celsius), userNumber 
  *
  *output:   result;
  *
@@ -23,7 +23,71 @@ namespace Question_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Unit Converter");
+            Console.ReadLine();
+
+            //prompt user input(unitConverter)
+            Console.WriteLine("Converter A: Pounds <--> Kilograms");
+            Console.WriteLine("Converter B: Fluid Ounces(U.S) <--> Liters");
+            Console.WriteLine("Converter C: Inches <--> Centimeters");
+            Console.WriteLine("Converter D: Fahrenheit <--> Celsius");
+            Console.Write("Pick which converter you want to use" +
+                            "(A, B, C, D): ");
+            char unitConverter = char.Parse(Console.ReadLine());
+            //Console.WriteLine(unitConverter);
+
+            //user input
+            double userNumber;
+
+            //prompt user which unit to convert based on unitConverter picked
+            switch (unitConverter)
+            {
+                //Pounds to kilogram vice versa
+                case 'a':
+                case 'A':
+                    Console.WriteLine("Option 1: Pounds --> Kilogram");
+                    Console.WriteLine("Option 2: Kilogram --> Pounds");
+
+                    Console.Write("Option: ");
+                    int option = int.Parse(Console.ReadLine());
+                    switch (option)
+                    {
+                        //if user looking for kilogram
+                        case 1:
+                            //prompt user for value of pounds
+                            Console.Write("Pounds: ");
+                            userNumber = double.Parse(Console.ReadLine());
+                            
+                            //calculate
+                            double kilogram = userNumber / 2.2046;
+                            //result
+                            Console.WriteLine($"{userNumber} pounds = {kilogram} " +
+                                              $"kilograms.");
+                            break;
+
+                        //if user looking for pounds
+                        case 2:
+                            //prompt user for value of kilogram
+                            Console.Write("Kilogram: ");
+                            userNumber = double.Parse(Console.ReadLine());
+                            
+                            //calculate
+                            double pounds = userNumber * 2.2046;
+
+                            //result
+                            Console.WriteLine($"{userNumber} kilograms = {pounds} " +
+                                              $"pounds.");
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+
+            }
+            
+
         }
     }
 }
