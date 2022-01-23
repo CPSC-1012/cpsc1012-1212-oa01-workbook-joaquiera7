2dec2c4367e2788aa034b3b0225f09d8d0a452e3
Jongie Oaquiera <joaquiera1@nait.ca>
2022-01-22 22:25:38 -0700

    -intial documentation done

--- Decision_Structrure_Programming_Exercises/Question_1/Program.cs
+++ Decision_Structrure_Programming_Exercises/Question_1/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*Purpose:  to flip a coin and let the user guess whether it's HEAD or TAIL and report if they
+ *          guess correctly.
+ * 
+ * Input:   userGuess
+ * 
+ * Output:  coinResult
+ * 
+ * Algorithm:1) prompt the user for their guess if HEAD or TAIL (userGuess)
+ *           2) generate random number (0 or 1)
+ *              > 0 = Head
+ *              > 1 = Tail
+ *           3) Display result as well as if the user guess correctly
+ * 
+ */
+using System;
 
 namespace Question_1
 {
