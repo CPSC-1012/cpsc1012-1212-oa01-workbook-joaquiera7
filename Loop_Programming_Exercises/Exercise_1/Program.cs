db1e97052d33543cbe9e6b8e1e966064434c11f6
Jongie Oaquiera <joaquiera1@nait.ca>
2022-02-20 15:00:56 -0700

    -initial commit
    -created initial documentation

--- Loop_Programming_Exercises/Exercise_1/Program.cs
+++ Loop_Programming_Exercises/Exercise_1/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*Purpose:  Write a program that allows the user to answer a randomly generated math question.
+ *          Program must be able to handle invalid inputs.
+ * 
+ *input:    userAnswer
+ *
+ *output:   result
+ *
+ *Algorithm:1)display randomly generated math question
+ *          2)prompt user for answer (userAnswer)
+ *          3)Check if userAnswer is a valid input. If not, prompt again for another answer.
+ *          4)Check if userAnswer is correct. Display result
+ */
+
+using System;
 
 namespace Exercise_1
 {
