e80558954f3a5fa33259d26bfd9fafe922b8f2b6
Jongie Oaquiera <joaquiera1@nait.ca>
2022-02-20 15:56:12 -0700

    -initial commit
    -initial documentation done

---
+++ Loop_Programming_Exercises/MathTutor02/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Purpose:  Modify Exercise_1 (MathTutor01) that allows for multiple attempts to answer the question.
+ *          When user answer the question correctly, display the number of atttempts made to answer the question.
+ * 
+ * input:    userAnswer
+ *
+ *output:   sum, numberOfAttempts
+ *
+ *
+ *Algorithm:1)display randomly generated math question
+ *          2)prompt user for answer (userAnswer)
+ *          3)Check if userAnswer is a valid input. If not, prompt again for another answer.
+ *          4)Check if userAnswer is correct. Display result
+ *          5)Display number of attempts made
+ * 
+ */
+
+using System;
+
+namespace MathTutor02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
