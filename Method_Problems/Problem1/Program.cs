4cb44e2232df6a3145c621804c588634efa350e3
Jongie Oaquiera <joaquiera1@nait.ca>
2022-02-20 14:55:41 -0700

    initial commit question 1

--- Method_Problems/Problem1/Program.cs
+++ Method_Problems/Problem1/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*Purpose: Create a program that will determine the cost of admission for a theatre using methods.
+ * 
+ * Input:  age
+ * 
+ * Output: admissionAmount
+ * 
+ * Algorithm:   1) Prompt for user age. (age)
+ *              2)create method "GetTicketPrice(int age)" that will return the price of the ticket based on age
+ *              3)Create method "DisplayMenu"
+ * 
+ */
+
+
+using System;
 
 namespace Problem1
 {
@@ -7,6 +20,24 @@ namespace Problem1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("Children 6 and under = Free ($0.00)");
+            Console.WriteLine("Students 7 to 17 = $9.80");
+            Console.WriteLine("Adults 18 to 54  = $11.35");
+            Console.WriteLine("Seniors 55+ = $10.00");
+        }
+        static int Age()
+        {
+            int age;
+            bool validInput = int.TryParse;
+
+            Console.Write("What is your current age?: ");
+            age = int.Parse(Console.ReadLine());
+
+            
         }
     }
 }
