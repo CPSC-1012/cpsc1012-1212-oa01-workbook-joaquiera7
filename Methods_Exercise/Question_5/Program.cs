febf2ec932dc2e2a4c772de2f65b45020315011f
Jongie Oaquiera <joaquiera1@nait.ca>
2022-03-07 21:46:00 -0700

    -initial documentation done for question 5

---
+++ Methods_Exercise/Question_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Cpsc1012;
+
+namespace Question_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Purpose:  - Write a program to keep track of revenue of a one-person taxi company. Must use menu.
+             * 
+             * Input:   distanceTravelled, timeOfTrip, fare, tip
+             * 
+             * Output:  totalGrossIncome
+             * 
+             */
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
