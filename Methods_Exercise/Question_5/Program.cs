2a0c658626a4216bc45dccd2af34e1b13851df65
Jongie Oaquiera <joaquiera1@nait.ca>
2022-03-07 23:58:56 -0700

    Question 5 done

--- Methods_Exercise/Question_5/Program.cs
+++ Methods_Exercise/Question_5/Program.cs
@@ -14,7 +14,107 @@ namespace Question_5
              * Output:  totalGrossIncome
              * 
              */
-            Console.WriteLine("Hello World!");
+
+            //user Input
+            int menuSelected = 0;
+
+            //result
+            double totalGrossIncome = 0;
+
+            //charges
+            double airportTrip = 0;
+            double regularFare = 0;
+            
+
+            while (menuSelected > 1 || menuSelected < 4)
+            {
+                Console.Clear();
+
+                Console.WriteLine($"{totalGrossIncome:c2}");
+                Console.WriteLine("Revenue Calculator");
+                Console.WriteLine();
+                Console.WriteLine("1 > Airport Trip");
+                Console.WriteLine("2 > Regular Fare");
+                Console.WriteLine("3 > Flat Rate");
+                Console.WriteLine("4 > Exit Program / End the day");    //display total gross income
+                menuSelected = Utilities.PromptForPositiveNumber(1, 4, "Enter a number 1-4: ");
+
+                switch (menuSelected)
+                {
+                    case 1:
+                        totalGrossIncome += CalculateAirportCharge();
+                        break;
+
+                    case 2:
+                        totalGrossIncome += CalculateRegularFareCharge();
+                        break;
+
+                    case 3:
+                        totalGrossIncome += CalculateFlatRate();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine($"Total gross income: {totalGrossIncome:c}");
+                        Console.WriteLine();
+                        Console.WriteLine("Exiting . . . .");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+            static double CalculateAirportCharge()
+            {
+                Console.Clear();
+                double chargePerTrip = 25;
+                double totalAirportCharge = 0;
+
+                Console.WriteLine($"Airport Charge: {chargePerTrip:c}");
+                double tip = Utilities.PromptForDoublePositiveOrZero("Tip: ");
+
+                //calculate total of airport fares
+                totalAirportCharge += (tip + chargePerTrip);
+                return totalAirportCharge;
+            }
+
+            static double CalculateRegularFareCharge()
+            {
+                double distanceTravelled = 0;
+                double timeOfTrip = 0;
+                double tip = 0;
+
+                double chargePerKM = 1.1;
+                double chargePerMin = 0.2;
+                double totalRegularFareCharge = 0;
+
+
+                Console.Clear();
+                distanceTravelled = Utilities.PromptForDoublePositiveOrZero("Enter distance travelled(in km): ");
+                timeOfTrip = Utilities.PromptForDoublePositiveOrZero("Enter time of trip(in minutes): ");
+                tip = Utilities.PromptForDoublePositiveOrZero("Enter tip: ");
+
+                //calculate regular fare
+                totalRegularFareCharge = (distanceTravelled * chargePerKM) + (timeOfTrip * chargePerMin) + tip;
+                return totalRegularFareCharge;
+            }
+
+            static double CalculateFlatRate()
+            {
+                double flatRateTotal = 0;
+                double flatRate = 0;
+                double tip = 0;
+
+                Console.Clear();
+                flatRate = Utilities.PromptForDoublePositiveOrZero("Enter flat rate: ");
+                tip = Utilities.PromptForDoublePositiveOrZero("Enter tip: ");
+
+                //calculate flat rate
+                flatRateTotal = flatRate + tip;
+                return flatRateTotal;
+            }
         }
+
+        
     }
 }
--- Methods_Exercise/Question_5/Utilities.cs
+++ Methods_Exercise/Question_5/Utilities.cs
@@ -9,7 +9,7 @@ namespace Cpsc1012
     public class Utilities
     {
         /// <summary>
-        /// This method will prompt and check input for positive integer number.
+        /// This method will prompt and check input for positive non - zero integer number.
         /// </summary>
         /// <param name="message"></param>
         /// <returns>number</returns>
@@ -30,6 +30,66 @@ namespace Cpsc1012
                     else
                     {
                         Console.WriteLine("Invalid input. Must be a positive non-zero number.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Input must be an integer number.");
+                }
+            }
+            return number;
+        }
+
+        /// <summary>
+        /// This method will prompt and check input for positive integer number that is between min and max.
+        /// </summary>
+        /// <param name="min">The minimum number allowed.</param>
+        /// <param name="max">The maximum number allowed.</param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        static public int PromptForPositiveNumber(int min, int max, string message)
+        {
+            int number = 0;
+            bool validNumber = false;
+
+            while (validNumber == false)
+            {
+                Console.Write(message);
+                if (int.TryParse(Console.ReadLine(), out number))
+                {
+                    if (number >= min && number <= max)
+                    {
+                        validNumber = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid input. Must enter a number between {min} and {max}.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Input must be an integer number.");
+                }
+            }
+            return number;
+        }
+        static public double PromptForDoublePositiveOrZero(string message)
+        {
+            double number = 0;
+            bool validNumber = false;
+
+            while (validNumber == false)
+            {
+                Console.Write(message);
+                if (double.TryParse(Console.ReadLine(), out number))
+                {
+                    if (number >= 0)
+                    {
+                        validNumber = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Must be a positive or zero number.");
                     }
                 }
                 else
