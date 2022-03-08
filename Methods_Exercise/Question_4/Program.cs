884aa488e2c6b4424d0c99f015810bda696f4256
Jongie Oaquiera <joaquiera1@nait.ca>
2022-03-07 21:23:06 -0700

    -initial documentation done for question 4

---
+++ Methods_Exercise/Question_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Question_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Purpose:  - Write a program to calculate and display the surface area and volume of a rectangle.
+             *          - Use the method CalculateArea() and CalculateVolume() which will passed in length and width.
+             *          - Validation required to ensure a negative value is not allowed.
+             *          
+             *Input:    length, width
+             *
+             *Output:   resultArea, resultVolume
+             * 
+             */
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
